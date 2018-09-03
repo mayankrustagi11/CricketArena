636f2c863f0a0c3767b6ddf0f9f510c1738d0661
rails -v <mayankrustagi11@gmail.com>
2018-09-03 16:15:37 +0530

    Schedule

---
+++ schedule.js
@@ -0,0 +1,27 @@
+window.onload = fetchSchedule;
+
+function fetchSchedule(e) {
+    e.preventDefault();
+
+    fetch(`http://cricapi.com/api/matchCalendar?apikey=${apikey}`)
+        .then(res => res.json())
+        .then(data => showSchedule(data.data))
+        .catch(err => console.log(err));
+}
+
+function showSchedule(matches) {
+    const results = document.getElementById("schedule");
+    results.innerHTML = "";
+
+    matches.forEach(match => {
+        const div = document.createElement("div");
+        div.classList.add("card", "card-body", "mb-3");
+        div.innerHTML = `
+            <h4>${match.name}</h4>
+            <p class="lead text-secondary">Date: ${match.date}</p>
+            <p class="lead text-secondary">Id: ${match.unique_id}</p>
+        `;
+
+        results.appendChild(div);
+    });
+}
